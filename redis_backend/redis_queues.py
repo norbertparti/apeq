40e85d9eafe14ebb3803b9f41ee60cdb1b7f85e5
Norbert Parti <norbertparti07@gmail.com>
2018-06-21 22:02:15 +0200

    Handle case: processing during maintain processing queue

--- redis_backend/redis_queues.py
+++ redis_backend/redis_queues.py
@@ -3,42 +3,11 @@ import time
 import redis
 
 
-class RedisQueueTopic:
-    """
-    Reliable message queue based on Redis
-    """
-
-    def _connect_to_redis(self, connection_pool):
-        return redis.StrictRedis(connection_pool=connection_pool)
-
-    def __init__(self, connection_pool, worker_id, name, processing_queue):
-        """
-        Args:
-            connection_pool: explicit connection pool to use
-            name: topic of messages
-            processing_queue: temporary queue for messages which are in processing
-        """
-        self.name = name
-        self.processing_queue = processing_queue
-        self.r = self._connect_to_redis(connection_pool)
-        self.worker_id = worker_id
-
-    def __iter__(self):
-        while True:
-            res = self.r.rpoplpush(src=self.name, dst=self.processing_queue)
-            if res == f'STOP:{self.worker_id}':
-                break
-            if res:
-                yield res
-            else:
-                time.sleep(0.1)
-                continue
+def _connect_to_redis(connection_pool):
+    return redis.StrictRedis(connection_pool=connection_pool)
 
 
-class ProcessingQueue:
-    def _connect_to_redis(self, connection_pool):
-        return redis.StrictRedis(connection_pool=connection_pool)
-
+class Queue:
     def __init__(self, connection_pool, worker_id, topic, processing_queue):
         """
         message format timestamp(unix epoch);message
@@ -49,8 +18,28 @@ class ProcessingQueue:
         """
         self.topic = topic
         self.processing_queue = processing_queue
-        self.r = self._connect_to_redis(connection_pool)
+        self.r = _connect_to_redis(connection_pool)
         self.worker_id = worker_id
+
+
+class TopicRedisQueue(Queue):
+    """
+    Reliable message queue based on Redis
+    """
+
+    def __iter__(self):
+        while True:
+            res = self.r.rpoplpush(src=self.topic, dst=self.processing_queue)
+            if res == f'STOP:{self.worker_id}':
+                break
+            if res:
+                yield res
+            else:
+                time.sleep(0.1)
+                continue
+
+
+class ProcessingQueue(Queue):
 
     def maintain(self, timeout):
         """
@@ -59,16 +48,25 @@ class ProcessingQueue:
             timeout: timeout in seconds
         """
         while True:
-            res = self.r.lpop(self.processing_queue)
+            res = self.r.rpoplpush(src=self.processing_queue, dst=self.processing_queue)  # rolling the queue
             if res:
                 encoded_res = str(res, encoding='utf-8')
                 if encoded_res == f'STOP:{self.worker_id}':
                     break
 
                 timestamp, value = encoded_res.split(';')
-                if time.time() - float(timestamp) > float(timeout):
-                    print(f'Pushed back: {value}')
-                    self.r.rpush(self.topic, f'{time.time()};{value}')
+                timed_out = time.time() - float(timestamp) > float(timeout)
+                if timed_out:
+                    processed = self._remove_from_processing(res)
+                    if not processed:
+                        self._push_back(value)
+                        print(f'Pushed back: {value}')
             else:
                 time.sleep(1)
                 continue
+
+    def _push_back(self, value):
+        self.r.rpush(self.topic, f'{time.time()};{value}')
+
+    def _remove_from_processing(self, res):
+        return self.r.lrem(name=self.processing_queue, count=0, value=res)
